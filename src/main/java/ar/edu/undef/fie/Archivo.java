31b2248cc728ee058e542462b33bbc27fa8d2a77
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-10-06 10:46:45 -0300

    first commit

---
+++ src/main/java/ar/edu/undef/fie/Archivo.java
@@ -0,0 +1,9 @@
+package ar.edu.undef.fie;
+
+public class Archivo {
+
+    private String contenido;
+    private String nombre;
+
+
+}
---
+++ src/main/java/ar/edu/undef/fie/Compressor.java
@@ -0,0 +1,8 @@
+package ar.edu.undef.fie;
+
+public class Compressor {
+    public static void main(String[] args) {
+
+        public
+    }
+}
